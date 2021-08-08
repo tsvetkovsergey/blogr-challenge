7a33dc53aa0b5add7d71199fd29f0015f5172926
Sergei Tsvetkov <tsvetkov2004@gmail.com>
2021-08-08 15:42:17 +0300

    Babel configure with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+
 module.exports = {
-  mode: "development",
+  mode,
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   plugins: [
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
@@ -18,17 +25,17 @@ module.exports = {
 
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: {
-      //     // without additional settings, this will reference .babelrc
-      //     loader: "babel-loader",
-      //   },
-      // },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          // without additional settings, this will reference .babelrc
+          loader: "babel-loader",
+        },
+      },
       {
         // It will work with scss, sass or css
-        test: /\.scss$/i,
+        test: /\.(s[ac]|c)ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader",
@@ -46,10 +53,5 @@ module.exports = {
         type: "asset/resource",
       },
     ],
-  },
-
-  devtool: "source-map",
-  devServer: {
-    contentBase: "./dist",
   },
 };
